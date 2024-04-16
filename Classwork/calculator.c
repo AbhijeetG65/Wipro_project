8544f7314ad8fe02dd1c50176df6a251bd8bfccd
Abhijeet gorai <112094445+AbhijeetG65@users.noreply.github.com>
2024-04-16 23:22:45 +0530

    Add files via upload

---
+++ Classwork/calculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a, b, sum, divide, mult, mod;
+    scanf("enter value of a and b %d%d",&a,&b);
+
+    sum = a + b;
+    printf("sum of a and b is: %d",sum);
+
+    mult = a*b;
+    printf("division of a and b is: %d", mult);
+
+    mod = a%b;
+    printf("division of a and b is: %d", mod);
+
+}
