5d8f8a2a326283a9aeef296073b506a46af00fdb
Abhijeet gorai <112094445+AbhijeetG65@users.noreply.github.com>
2024-04-19 10:03:34 +0530

    Add files via upload

---
+++ Classwork/day07/file1.c
@@ -0,0 +1,21 @@
+/*
+demo on pointers
+*/
+
+#include<stdio.h>
+int main(){
+    int qty=100;
+    int *ptr;
+
+    printf("\n Address of qty = %u and its value=%u", &qty,qty);
+    printf("\n Address of qty = %u and its value=%u", &ptr,ptr);
+
+    ptr = &qty;
+
+    printf("\n Address of qty = %u and its value=%u", &qty,qty);
+    printf("\n Address of qty = %u and its value=%u", &ptr,ptr);
+
+    printf("\n\n");
+    return 0;
+
+}
